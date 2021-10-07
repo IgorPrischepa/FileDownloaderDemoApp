36b42d84318966e551c61b7b2afeceaef8f558ed
Prischepa Igor <i.a.prishepa@gmail.com>
2021-10-07 22:31:09 +0300

    Add project files.

---
+++ FileDownloader/Contract/IFileDownloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloader.Contract
+{
+    public interface IFileDownloader
+    {
+        void AddFileToDownloadingQueue(string fileId, string url, string pathToSave);
+
+        event Action<string> OnDownloaded;
+
+        event Action<string, Exception> OnFailed;
+    }
+}
---
+++ FileDownloader/Implementation/FileDownloader.cs
@@ -0,0 +1,20 @@
+﻿using FileDownloader.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloader.Implementation
+{
+    class FileDownloader : IFileDownloader
+    {
+        public event Action<string> OnDownloaded;
+        public event Action<string, Exception> OnFailed;
+
+        public void AddFileToDownloadingQueue(string fileId, string url, string pathToSave)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
