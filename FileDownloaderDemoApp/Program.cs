2c56496848737d926912847b4741e649d479b684
Prischepa Igor <i.a.prishepa@gmail.com>
2021-10-15 20:26:21 +0300

    Adding lock to update info

--- FileDownloaderDemoApp/Program.cs
+++ FileDownloaderDemoApp/Program.cs
@@ -12,15 +12,13 @@ namespace FileDownloaderDemoApp
         public static int downloadedCount = 0;
         public static int failedCount = 0;
 
-        private static object syncDownloadCounter = new();
-        private static object syncFailedCounter = new();
-        private static object syncAcccessToCounter = new();
+        private static object syncAcccessToCounters = new();
 
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
         static void Main()
         {
-            //Console.WriteLine("To interrupt download process and exit, please press any key");
+            Console.WriteLine("To interrupt download process and exit, please press any key");
 
             var defaultPathToSave = "Downloaded";
             var defaultPathToListOfUrls = "download_list.txt";
@@ -31,7 +29,7 @@ namespace FileDownloaderDemoApp
             EnsureDirectoryCreated(defaultPathToSave);
 
             var fileDownloader = new Downloader.FileDownloader();
-            fileDownloader.SetDegreeOfParallelism(10);
+            fileDownloader.SetDegreeOfParallelism(5);
             fileDownloader.OnDownloaded += SucceessDownload;
             fileDownloader.OnFailed += FailedDownload;
 
@@ -52,11 +50,6 @@ namespace FileDownloaderDemoApp
 
         private static void FailedDownload(string filePath, Exception exception)
         {
-            lock (syncFailedCounter)
-            {
-                failedCount++;
-            }
-
             Console.WriteLine($"{exception.Message}");
 
             logger.Error($"{exception.Message}");
@@ -66,35 +59,41 @@ namespace FileDownloaderDemoApp
                 logger.Error($"{exception.StackTrace}");
             }
 
-            UpdateInfoInCosole();
-
+            lock (syncAcccessToCounters)
+            {
+                failedCount++;
+                UpdateInfoInCosole();
+            }
         }
 
         private static void SucceessDownload(string obj)
         {
-            lock (syncDownloadCounter)
+            lock (syncAcccessToCounters)
             {
                 downloadedCount++;
+
+                Console.WriteLine($"{obj} downloaded");
+
+                UpdateInfoInCosole();
             }
-
-            Console.WriteLine($"{obj} downloaded");
-
-            UpdateInfoInCosole();
 
         }
 
         private static void UpdateInfoInCosole()
         {
-            var progress = ((downloadedCount + failedCount) * 100) / fileCount;
-
-            Console.Title = $"Total:{fileCount} Downloaded:{downloadedCount} Failed:{failedCount} Progress: {progress}%";
-
-            if (progress == 100)
+            lock (syncAcccessToCounters)
             {
-                Console.WriteLine($"Precceded all urls. Successfuly download: {downloadedCount}. Failed: {failedCount}");
-                Console.WriteLine("Press any key to exit.");
-            }
+                var progress = ((downloadedCount + failedCount) * 100) / fileCount;
 
+                Console.Title = $"Total:{fileCount} Downloaded:{downloadedCount} Failed:{failedCount} Progress: {progress}%";
+
+                if (progress == 100)
+                {
+                    Console.WriteLine($"Precceded all urls. Successfuly download: {downloadedCount}. Failed: {failedCount}");
+                    Console.WriteLine("Press any key to exit.");
+                }
+
+            }
         }
 
         private static void EnsureDirectoryCreated(string path)
